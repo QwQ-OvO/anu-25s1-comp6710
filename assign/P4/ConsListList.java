51c299a4fe1258c33f1b2a17c5bec7651e20dce7
siyanliu <u8000945@anu.edu.au>
2025-04-22 20:45:21 +1000

    P4 part 1

---
+++ assign/P4/ConsListList.java
@@ -0,0 +1,198 @@
+package P4;
+
+import java.util.*;
+
+public class ConsListList<T> implements List<T> {
+
+    // 定义Node<T>内部类. 创建基于节点的链表
+    public static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node<T> first;
+    private int size;
+
+    public ConsListList() {
+        this.first = null;
+        this.size = 0;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+
+        Node<T> newNode = new Node<>(t,null);
+
+        if (first == null) {
+            first = newNode;
+
+        } else {
+            Node<T> currentNode = first;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        size++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (size == 0) {
+            return false;
+        }
+
+        if (Objects.equals(o, first.value)) {
+            first = first.next;
+            size--;
+            return true;
+        }
+
+        Node<T> currentNode = first;
+
+        while (currentNode.next != null) {
+            if (Objects.equals(o, currentNode.next.value)) {
+                currentNode.next = currentNode.next.next;
+                size--;
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return first.value;
+        }
+        Node<T> currentNode = first;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.value;
+    }
+
+    @Override
+    public T set(int index, T element) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw  new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            T previousFirstValue = first.value;
+            first.value = element;
+            return previousFirstValue;
+        }
+        Node<T> currentNode = first;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        T previousValue = currentNode.value;
+        currentNode.value = element;
+        return previousValue;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return List.of();
+    }
+}
